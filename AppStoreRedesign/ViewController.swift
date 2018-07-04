a7b6c5a8369312225d413fc977ad00f5e0d90511
hkbenchan <benchanhp@gmail.com>
2018-07-05 02:09:31 +0800

    Initial project setup with swift environment

---
+++ AppStoreRedesign/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppStoreRedesign
+//
+//  Created by Chan Ho Pan on 5/7/2018.
+//  Copyright © 2018 hpchan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
